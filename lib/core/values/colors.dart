2c050d08143fd4e18e67922e0e366dae3c1b4b82
smrTofighi <mrtofxn@gmail.com>
2023-04-15 05:59:50 -0400

    create SolidColors class for Colors

---
+++ lib/core/values/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class SolidColors {
+  static const Color primary = Color.fromARGB(255, 2, 207, 36);
+  static const Color pressed = Color.fromARGB(255, 2, 240, 36);
+}
