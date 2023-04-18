08353d5d5dcaa766b3efce4102b14f129081bb82
smrTofighi <mrtofxn@gmail.com>
2023-04-18 07:48:27 -0400

    add GestureWidget

--- lib/pages/widgets/back_button.dart
+++ lib/pages/widgets/back_button.dart
@@ -10,20 +10,23 @@ class MyBackButton extends StatelessWidget {
     return Positioned(
       top: Dimens.medium,
       left: Dimens.medium,
-      child: Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(100),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.4),
-                  blurRadius: 18,
-                  offset: const Offset(2, 3)),
-            ],
-          ),
-          child: const Icon(Icons.arrow_back)),
+      child: GestureDetector(
+        onTap: onPressed,
+              child: Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.4),
+                    blurRadius: 18,
+                    offset: const Offset(2, 3)),
+              ],
+            ),
+            child: const Icon(Icons.arrow_back)),
+      ),
     );
   }
 }
