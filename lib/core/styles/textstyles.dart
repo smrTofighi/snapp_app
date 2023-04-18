8b020a27c2ddee803a8d49db05c8c849ca8d6431
smrTofighi <mrtofxn@gmail.com>
2023-04-18 07:47:52 -0400

    change variable name

--- lib/core/styles/textstyles.dart
+++ lib/core/styles/textstyles.dart
@@ -3,5 +3,5 @@ import 'package:google_fonts/google_fonts.dart';
 
 class MyTextStyles {
   MyTextStyles._();
-  static TextStyle bottom = GoogleFonts.vazirmatn(fontSize: 20);
+  static TextStyle botton = GoogleFonts.vazirmatn(fontSize: 20);
 }
